a5bc0c9eab46485b8e1045f2e3750b07aead7437
KirkNotCaptain <cmariekirk@gmail.com>
2021-03-29 19:41:22 -0400

    Installed web socket client and server server

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
-import "./App.css";
+import './App.css';
+import { useEffect } from 'react';
+import { w3cwebsocket as W3CWebSocket } from 'websocket';
+
+const client = new W3CWebSocket('ws://127.0.0.1:8000');
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<h1>Hello World</h1>
-			</header>
-		</div>
-	);
+  useEffect(() => {
+    client.onopen = () => {
+      console.log('WebSocket Client Connected');
+    };
+    client.onmessage = (message) => {
+      console.log(message);
+    };
+  }, []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,20 +1,19 @@
 const webSocketServerPort = 8000;
-const WebSocketServer = require('websocket').server;
+const webSocketServer = require('websocket').server;
 
 const http = require('http');
 const server = http.createServer();
 server.listen(webSocketServerPort);
 console.log('Websocket server listening on localhost://', webSocketServerPort);
 
-const wsServer = new webSocketServerPort({
+const wsServer = new webSocketServer({
   httpServer: server,
 });
 
 const clients = {};
-const clientId = 0;
-const clients = {};
+let clientId = 0;
 
-wsServer.on('request', (req) => {
+wsServer.on('request', (request) => {
   console.log('Received new connection from client: ', clientId);
   clientId++;
 
