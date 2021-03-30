b7f76939d6db47cef15d718a3bdc3ef6d8269945
KirkNotCaptain <cmariekirk@gmail.com>
2021-03-29 20:01:30 -0400

    Connected to TMDB API from client

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
+import 'axios';
+import API_KEY from './config.js';
 import { useEffect } from 'react';
 import { w3cwebsocket as W3CWebSocket } from 'websocket';
+import axios from 'axios';
 
 const client = new W3CWebSocket('ws://127.0.0.1:8000');
 
@@ -9,6 +12,11 @@ function App() {
     client.onopen = () => {
       console.log('WebSocket Client Connected');
     };
+    axios
+      .get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`)
+      .then((data) => {
+        console.log(data);
+      });
     client.onmessage = (message) => {
       console.log(message);
     };
